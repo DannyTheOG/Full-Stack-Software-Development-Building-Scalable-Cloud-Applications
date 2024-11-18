9e13c6b5bc26438c891a59d255898b35f02ebcf6
DannyTheOG <nii.ayi.97.na@gmail.com>
2024-11-18 16:19:00 +0000

    wk7 guided project initialization

---
+++ wk7/guided project/02_Javascript ES6 & OOP Part 1 Guided Project_Source Code/script.js
@@ -0,0 +1,20 @@
+function initialise() {
+
+}
+
+// accept user income
+
+// accept expense, expense description and amount
+const expenseCategories = [
+    "Groceries",
+    "Entertainment",
+    "Rent",
+    "Utilities",
+    "Health",
+    "Education",
+    "Miscellaneous"
+]
+
+// display a list of all expense transactions ( expense, description and amount )
+
+// calculate and display summary
