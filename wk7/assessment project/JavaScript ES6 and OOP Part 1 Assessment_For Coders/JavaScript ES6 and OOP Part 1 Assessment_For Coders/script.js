01b9892a167af97c919607aaf77c98a1e8e3c2cc
DannyTheOG <nii.ayi.97.na@gmail.com>
2024-11-22 01:33:38 +0000

    wk7 assessment project completed - recipe book app

--- wk7/assessment project/JavaScript ES6 and OOP Part 1 Assessment_For Coders/JavaScript ES6 and OOP Part 1 Assessment_For Coders/script.js
+++ wk7/assessment project/JavaScript ES6 and OOP Part 1 Assessment_For Coders/JavaScript ES6 and OOP Part 1 Assessment_For Coders/script.js
@@ -1,5 +1,10 @@
 const recipes = [];
 
+let titleInput = document.getElementById( 'title' );
+let ingredientsInput = document.getElementById( 'ingredients' );
+let instructionsInput = document.getElementById( 'instructions' );
+let recipesList = document.getElementById( 'recipes' );
+
 //Declared index of the recipe to be edited
 var indexOfRecipeToBeEdited = -1
 
@@ -14,29 +19,79 @@ var isEditMode = false
 // Finally, display the recipes using the displayRecipes() function
 document.getElementById('add-recipe-btn').addEventListener('click', function() {
     // Write your code here for task 1
+    let recipe = {
+        title: titleInput.value,
+        ingredients: ingredientsInput.value,
+        instructions: instructionsInput.value
+    }
+    const { title, ingredients, instructions } = recipe;
+
+    if( title && ingredients && instructions ) {
+        if( !isEditMode ) 
+            addRecipe( recipe )
+        else {
+            if ( indexOfRecipeToBeEdited >= 0 && indexOfRecipeToBeEdited < recipes.length ) 
+                recipes[indexOfRecipeToBeEdited] = recipe;
+            
+            isEditMode = false;
+        }
+
+        clearInputFields()
+        displayRecipes()
+
+    } else {
+        alert("Please fill out all fields.");
+        return;
+    }
 });
 
 // Clear the form's input fields
 function clearInputFields() {
     // Write your code here for task 2
+    titleInput.value = "";
+    ingredientsInput.value = "";
+    instructionsInput.value = "";
     
 }
 
 // Add the new recipe to the recipes array
 function addRecipe(recipe) {
     // Write your code here for task 3
-    
+    recipes.push( recipe )
 }
 
 // Display Recipes
 function displayRecipes() {
     // Write your code here for task 4
-       
+    let recipesListHTML = "";
+
+    for ( let [index, recipe] of recipes.entries() ) {
+        const { title, ingredients, instructions } = recipe;
+        recipesListHTML += `
+            <li>
+                <p><b>${title}</b></p>
+                <p><b>ingredients:</b> ${ingredients}</p>
+                <p><b>instructions:</b> ${instructions}</p>
+                <button onclick = "editRecipe(${index});">EDIT</button>
+                <button onclick = "deleteRecipe(${index});">DELETE</button>
+            </li>
+        `
+    }
+
+    recipesList.innerHTML = recipesListHTML;
 }
 
 // Edit the recipe object when the Edit button is clicked
 function editRecipe(index) {
     // Write your code here for task 5
+    const { title, ingredients, instructions } = recipes[index];
+
+    titleInput.value = title;
+    ingredientsInput.value = ingredients;
+    instructionsInput.value = instructions;
+
+    isEditMode = true;
+    indexOfRecipeToBeEdited = index;
 }
 
 // Delete the recipe object when the Delete button is clicked
