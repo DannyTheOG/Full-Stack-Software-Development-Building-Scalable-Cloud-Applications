8af62fd64380dba4c0d7ec4cf8d9e11fa8ab9a8f
DannyTheOG <nii.ayi.97.na@gmail.com>
2024-12-19 23:13:09 +0000

    wk10 assessment project completed

--- wk10/assessment project/Data Structures using JavaScript Assessment_For Coders_Source Code/Data Structures using JavaScript Assessment_For Coders/queue.js
+++ wk10/assessment project/Data Structures using JavaScript Assessment_For Coders_Source Code/Data Structures using JavaScript Assessment_For Coders/queue.js
@@ -2,33 +2,47 @@
 class PrinterQueue {
   constructor() {
     // Initialize an empty array to store documents in the queue
+    this.items = [];
   }
 
   isEmpty() {
     // Check if the printer queue is empty
+    return this.items.length === 0;
   }
 
   // Add a new document to the printer queue
   enqueue(document) {
     // Push the specified document onto the end of the queue
     // Log a message indicating that the document has been added to the printer queue
+    this.items.push( document );
+    console.log( `${document} added to Printer Queue` );
   }
 
   // Remove and return the first document from the printer queue
   dequeue() {
     // Check if the queue is empty - Log a message indicating that the printer queue is empty
     // Remove and return the first document from the queue
+    if( !this.isEmpty() ) return this.items.shift();
+    else return null;
   }
 
   // Retrieve the first document in the printer queue without removing it
   peek() {
     // Check if the queue is empty - Log a message indicating that the printer queue is empty
     // Return a message indicating the next document to print
+    if( this.isEmpty() ) console.log( "Printer Queue is empty" );
+    else {
+      const document = this.items[0]
+      return `Next document to be printed: ${document}`;
+    }
   }
 
   // Display all documents in the printer queue
   displayQueue() {
     // Log each document in the queue on a new line
+    for (const item of this.items) {
+      console.log( item )
+    }
   }
 }
 
@@ -55,6 +69,7 @@ myPrinterQueue.displayQueue();
 function countStudentsUnableToEat(students, sandwiches) {
   // Create a counter to store the number of students who are unable to eat
   // Initialise this with 0
+  let unableToEat = 0;
 
   // Run a while loop to check if the student queue is non empty
   while(students.length > 0){
@@ -65,13 +80,24 @@ function countStudentsUnableToEat(students, sandwiches) {
      // Else
         // The student goes to the end of the queue
         // Increment unableToEat counter
+      const student = students.shift();
+      const sandwich = sandwiches[0];
+
+      if( student === sandwich ) {
+        sandwiches.shift();
+        unableToEat = 0;
+      } else {
+        students.push( student );
+        unableToEat += 1;
+      }
     
     // If all students are unable to eat, i.e., unableToEat is equal to the length of students array
-        // Return the number of students who are unable to eat
+      // Return the number of students who are unable to eat
+      if( unableToEat === students.length ) return unableToEat;
   }
   // After the while loop
   // Return the number of students who are unable to eat
-  
+  return unableToEat;
 }
 
 // Example usage:
--- wk10/assessment project/Data Structures using JavaScript Assessment_For Coders_Source Code/Data Structures using JavaScript Assessment_For Coders/stack.js
+++ wk10/assessment project/Data Structures using JavaScript Assessment_For Coders_Source Code/Data Structures using JavaScript Assessment_For Coders/stack.js
@@ -2,32 +2,39 @@
 class Stack {
   constructor() {
     // Initialize an empty array to store stack elements
+    this.items = [];
   }
 
   isEmpty() {
     // Check if the stack is empty
+    return this.items.length === 0;
   }
 
   // Push element onto the stack
   push(element) {
     // Add the specified element to the end of the items array
+    this.items.push( element );
   }
 
   // Pop element from the stack
   pop() {
     // Check if the stack is empty - If empty, return "Underflow"
     // Remove and return the last element of the items array
+    if( this.isEmpty() ) return "Underflow";
+    else return this.items.pop();
   }
 
   // Peek the top element of the stack
   peek() {
     // Return the last element of the items array without removing it
+    return this.items[ this.items.length - 1 ];
   }
 
   // Print the stack elements
   printStack() {
     // Iterate through each element in the items array
     // Return the resulting string with trailing spaces removed
+    return this.items.map( item =>  item )
   }
 }
 
@@ -56,19 +63,35 @@ console.log("Top element:", stack.peek());
 //Problem Statement 2
 function isBalanced(expression) {
   // Initialize an empty array to represent the stack
+  let stack = [];
 
   for (let i = 0; i < expression.length; i++) {
     //Iterate through each character in the expression
     // If it's an opening brace, push it onto the stack
     // If it's a closing brace, check if the stack is empty or if the top of the stack matches the corresponding opening brace
     // If the stack is empty or the top of the stack doesn't match, the expression is unbalanced
+    let character = expression[i];
+
+    if( character === "{" || character === "(" || character === "[" ) {
+      stack.push( character );  
+    }  else if( ( stack.length > 0 ) && isMatchingPair( stack[stack.length - 1], character ) ) {
+      stack.pop();
+    } else return false;
+
   }
 
   // If the stack is empty at the end, the expression is balanced
+  return stack.length === 0;
 }
 
 function isMatchingPair(opening, closing) {
   // Check if the opening and closing braces form a matching pair
+  if( 
+    ( opening === "{" && closing === "}" ) ||
+    ( opening === "(" && closing === ")" ) ||
+    ( opening === "[" && closing === "]" )
+  ) return true; 
+  else return false;
 }
 
 // Example usage:
