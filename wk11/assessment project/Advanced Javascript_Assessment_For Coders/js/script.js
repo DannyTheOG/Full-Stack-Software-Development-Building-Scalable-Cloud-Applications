5dc39727a4f35b2cc58279a1c3a4133a77de414b
DannyTheOG <nii.ayi.97.na@gmail.com>
2025-01-03 00:51:30 +0000

    wk11 assessment project completed

--- wk11/assessment project/Advanced Javascript_Assessment_For Coders/js/script.js
+++ wk11/assessment project/Advanced Javascript_Assessment_For Coders/js/script.js
@@ -2,7 +2,25 @@
 function register() {
   // Write your code for task1 here
   // Use fetchRegisterPageData() function to fetch values
+
+  // get the message element from the register.html page
+  const messageElement = document.getElementById("message");
   
+  // call the fetchRegisterPageData() function to fetch the form data
+  const userData = fetchRegisterPageData();
+  const { fullname, username, email, contact, password, confirmPassword } = userData;
+
+  // check if the password and confirm password match
+  if ( password !== confirmPassword ) {
+    messageElement.textContent = "Passwords do not match";
+    return;
+  }
+
+  // store the user data in the local storage
+  localStorage.setItem( "user", JSON.stringify( userData ) );
+
+  // redirect to the signin.html page
+  loadSignInPage();
 }
 
 // Fetch form data from the register.html page
@@ -22,6 +40,36 @@ function fetchRegisterPageData() {
 function signIn() {
   // Write your code for task2 here
   // Use fetchSignInPageData() function to fetch values
+
+  // get details from the signin form
+  const { username, password, messageElement } = fetchSignInPageData();
+
+  // check if the username and password are empty
+  if( username === "" || password === "" ) {
+    messageElement.textContent = "Please enter all the details";
+    return;
+  } else{
+    try {
+        // check if the user exists in the local storage
+        const registeredUser = JSON.parse( localStorage.getItem( "user" ) ); 
+
+        if( username === registeredUser.username && password === registeredUser.password ) {
+          // store the login status in the session storage
+          sessionStorage.setItem( "login", true );
+  
+          // redirect to the index.html page
+          window.location.href = "../html/index.html";
+        } else {
+          messageElement.textContent = "Invalid username or password";
+  
+          // set the login status to false
+          sessionStorage.setItem( "login", false );
+        }
+    } catch ( error ) {
+      messageElement.textContent = "The user is not registered yet. Please click on the register button to register";
+      
+    }
+  }
   
 }
 
@@ -39,6 +87,7 @@ function fetchSignInPageData() {
 function loadContent() {
   // Fetch the login info from the session storage
   const userLogin = sessionStorage.getItem("login");
+  console.log( userLogin )
 
   // Check whether the login status is true
   if (userLogin) {
@@ -91,20 +140,48 @@ function loadSignInPage() {
 // Function to fetch data from an external URL endpoint
 async function fetchData(url, listElement) {
   // Write your code for task3 here
-    
-   
+  
+  // fetch the data from the API URL
+  try {
+    const response = await( fetch( url ) );
+    const data = await( response.json() );
+
+    // if data is fetched successfully, display the data on the webpage
+    if( data ) {
+      displayData( data, listElement );
+    }
+
+  } catch ( error ) {
+    alert( "Error fetching data from the API" );
   }
+   
+}
 
 
 // Display data which is fetched from an external API
 function displayData(data, listElement) {
   // Write your code for task4 here
   
+  // display the data on the webpage
+  data.data.forEach( item => {
+    listElement.innerHTML += `
+      <div>
+        <img src = "${item.image}" alt = "${item.name}" style="width:100%; height:10vh;"/>
+        <h4>${item.name}</h4>
+      </div>
+    `;
+  })
   
 }
 
 // Logging out when clicked on the logout button
 function logout(){
   // Write your code for task5 here
+
+  // set login status to false
+  sessionStorage.setItem( "login", false );
+
+  // redirect to the signin.html page
+  loadSignInPage();
     
 }
