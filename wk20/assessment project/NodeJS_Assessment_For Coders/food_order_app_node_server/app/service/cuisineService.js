c6c9820c86d4c3a2f79eff16e7d5b83010275808
DannyTheOG <nii.ayi.97.na@gmail.com>
2025-06-25 22:14:36 +0100

    wk20 assessment project completed

---
+++ wk20/assessment project/NodeJS_Assessment_For Coders/food_order_app_node_server/app/service/cuisineService.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+
+class Cuisines {
+  constructor() {
+    this.cuisines = JSON.parse(fs.readFileSync("./app/data/cuisines.json", "utf-8"));
+  }
+  //COMPLETE TASK 1(b) HERE
+
+  getAllCuisines() {
+    return this.cuisines;
+  }
+
+  getCuisineById(id) {
+    const cuisine = this.cuisines.find((cuisine) => cuisine.id == id);
+    return cuisine;
+  }
+
+  getCuisineByName(name) {
+    const cuisine = this.cuisines.find((cuisine) => cuisines.name == name);
+    return cuisine;
+  }
+}
+
+module.exports = Cuisines;
